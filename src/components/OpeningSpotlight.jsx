664037be2213ff86acc9c15505de8662b6098d60
Kiran Nepali <itsmekiran44@gmail.com>
2024-04-15 17:03:56 +0545

    try again

--- src/components/OpeningSpotlight.jsx
+++ src/components/OpeningSpotlight.jsx
@@ -7,9 +7,9 @@ import PlanetImg from "../assets/planet.png";
 import { Link } from "react-scroll";
 
 export default function OpeningSpotlight() {
-  useEffect(() => {
-    OpeningAnim();
-  }, []);
+  // useEffect(() => {
+  //   OpeningAnim();
+  // }, []);
   const scroll = useRef(null);
   useGSAP(() => {
     gsap.to(scroll.current, {
@@ -22,7 +22,13 @@ export default function OpeningSpotlight() {
   });
   return (
     <div className="fixed top-0 left-0  w-full h-screen">
-      <div id="container" className="absolute w-full h-screen"></div>
+      <div id="container" className="absolute w-full h-screen">
+        <img
+          className="object w-full h-full object-cover object-center"
+          src="https://i.pinimg.com/originals/c2/91/0d/c2910d9584868f67b910834f98997f1d.jpg"
+          alt=""
+        />
+      </div>
       <img
         className="earth w-[10rem] h-[10rem]  md:w-[11rem] md:h-[11rem] absolute top-0 left-0 right-0 bottom-0 m-auto z-0 "
         src={PlanetImg}
@@ -46,7 +52,7 @@ export default function OpeningSpotlight() {
       </Link>
 
       {/* text  */}
-      <div className="absolute leading-none flex-col  bottom-[14%] left-[50%]  -translate-x-[50%] flex justify-center items-center">
+      <div className="absolute w-full leading-none flex-col  bottom-[14%] left-[50%]  -translate-x-[50%] flex justify-center items-center">
         <h1 className="uppercase welcome font-semibold tracking-wide text-nowrap text-[1.5rem] md:text-[3vw] text-zinc-100">
           Welcome to personify
         </h1>
